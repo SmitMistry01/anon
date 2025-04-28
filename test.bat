c3086ba6b1198de5d4f525bceb1acc3dbf3bd4e7
SmitMistry01 <smitmistry36466@gmail.com>
2025-04-29 00:13:33 +0530

    added

---
+++ test.bat
@@ -0,0 +1,17 @@
+@echo off
+
+:: Wait for the website to be up
+timeout /t 10 /nobreak >nul
+
+:: Check if the website is accessible
+for /f "delims=" %%r in ('curl -s -o nul -w "%%{http_code}" http://localhost:80') do (
+    set "response=%%r"
+)
+
+if "%response%"=="200" (
+    echo Website is accessible
+    exit /b 0
+) else (
+    echo Website is not accessible. Response code: %response%
+    exit /b 1
+)
